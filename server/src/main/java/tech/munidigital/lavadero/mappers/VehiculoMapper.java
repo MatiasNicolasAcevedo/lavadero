4c5f515e5e05c9b503522d12626e7ce583896a6b
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2025-02-22 18:36:25 -0300

    feat: se agrega dependencia mapstruct

---
+++ server/src/main/java/tech/munidigital/lavadero/mappers/VehiculoMapper.java
@@ -0,0 +1,20 @@
+package tech.munidigital.lavadero.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import tech.munidigital.lavadero.dto.request.VehiculoRequestDTO;
+import tech.munidigital.lavadero.dto.response.VehiculoResponseDTO;
+import tech.munidigital.lavadero.entity.Vehiculo;
+
+@Mapper(componentModel = "spring")
+public interface VehiculoMapper {
+
+    @Mapping(target = "clienteId", source = "cliente.id")
+    VehiculoResponseDTO toDto(Vehiculo vehiculo);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cliente", ignore = true)
+    @Mapping(target = "turnos", ignore = true)
+    Vehiculo toEntity(VehiculoRequestDTO dto);
+
+}
