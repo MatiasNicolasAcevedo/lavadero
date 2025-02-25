068d493c094b4a96d8465ec757cbc182801796ac
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2025-02-24 21:28:24 -0300

    feat: se agregan VehiculoController, VehiculoService y VehiculoService

---
+++ server/src/main/java/tech/munidigital/lavadero/service/VehiculoService.java
@@ -0,0 +1,13 @@
+package tech.munidigital.lavadero.service;
+
+import tech.munidigital.lavadero.dto.request.VehiculoRequestDTO;
+import tech.munidigital.lavadero.dto.response.VehiculoResponseDTO;
+import java.util.List;
+
+public interface VehiculoService {
+
+    VehiculoResponseDTO createVehiculo(VehiculoRequestDTO vehiculoRequestDTO);
+    VehiculoResponseDTO getVehiculoById(Long id);
+    List<VehiculoResponseDTO> getVehiculosByClienteId(Long clienteId);
+
+}
