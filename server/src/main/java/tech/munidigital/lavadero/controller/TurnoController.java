13725b25cf60257fff0e139692910ec1cafdcdeb
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2025-02-24 23:27:59 -0300

    feat: se agregan metodos y endpoint para cambiar estado de turno

--- server/src/main/java/tech/munidigital/lavadero/controller/TurnoController.java
+++ server/src/main/java/tech/munidigital/lavadero/controller/TurnoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.munidigital.lavadero.dto.request.TurnoRequestDTO;
 import tech.munidigital.lavadero.dto.response.TurnoResponseDTO;
+import tech.munidigital.lavadero.entity.enums.EstadoTurno;
 import tech.munidigital.lavadero.service.TurnoService;
 import java.util.List;
 
@@ -35,6 +36,20 @@ public class TurnoController {
     @GetMapping("/vehiculo/{vehiculoId}")
     public ResponseEntity<List<TurnoResponseDTO>> getTurnosByVehiculoId(@PathVariable Long vehiculoId) {
         List<TurnoResponseDTO> response = turnoService.getTurnosByVehiculoId(vehiculoId);
+        return ResponseEntity.ok(response);
+    }
+
+    // Endpoint para actualizar el turno a EN_PROCESO
+    @PutMapping("/{id}/en-proceso")
+    public ResponseEntity<TurnoResponseDTO> setTurnoEnProceso(@PathVariable Long id) {
+        TurnoResponseDTO response = turnoService.updateTurnoEstado(id, EstadoTurno.EN_PROCESO);
+        return ResponseEntity.ok(response);
+    }
+
+    // Endpoint para actualizar el turno a FINALIZADO
+    @PutMapping("/{id}/finalizado")
+    public ResponseEntity<TurnoResponseDTO> setTurnoFinalizado(@PathVariable Long id) {
+        TurnoResponseDTO response = turnoService.updateTurnoEstado(id, EstadoTurno.FINALIZADO);
         return ResponseEntity.ok(response);
     }
 
